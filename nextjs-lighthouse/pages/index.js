66eb96d13bd4976005aead11e9a8d8ea59adc2f6
Chris Van Horn <cvanhorn293@gmail.com>
2026-06-25 21:05:46 -0500

    Add initial assets and styles for the Next.js application
    - Added favicon.ico for the application icon.
    - Included large-image.jpg for use in the application.
    - Created robots.txt to disallow all web crawlers.
    - Added vercel.svg for branding purposes.
    - Introduced Home.module.css for component-specific styles.
    - Created global.css for global styles and reset.

---
+++ nextjs-lighthouse/pages/index.js
@@ -0,0 +1,101 @@
+import { useState } from 'react';
+
+import Fuse from 'fuse.js';
+import _ from 'lodash';
+
+import { countries } from '../countries';
+import styles from '../styles/Home.module.css';
+import CodeSampleModal from '../components/CodeSampleModal';
+
+export default function Start({ countries }) {
+  const [results, setResults] = useState(countries);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const fuse = new Fuse(countries, {
+    keys: ['name'],
+    threshold: 0.3,
+  });
+
+  return (
+    <div>
+      <main className={styles.container}>
+        <div className={styles.title}>
+          Powered by <a href="https://nextjs.org"></a>
+        </div>
+
+        <div className={styles.heroImage}>
+          <img src="large-image.jpg" />
+        </div>
+
+        <div>
+          <div className={styles.secondaryHeading}>Population Lookup</div>
+          <input
+            type="text"
+            placeholder="Country search..."
+            className={styles.input}
+            onChange={async (e) => {
+              const { value } = e.currentTarget;
+
+              const searchResult = fuse
+                .search(value)
+                .map((result) => result.item);
+
+              const updatedResults = searchResult.length
+                ? searchResult
+                : countries;
+              setResults(updatedResults);
+
+              // Fake analytics hit
+              console.info({
+                searchedAt: _.now(),
+              });
+            }}
+          />
+
+          <ul className={styles.countries}>
+            {results.map((country) => (
+              <li key={country.cca2} className={styles.country}>
+                <p>
+                  {country.name} - {country.population.toLocaleString()}
+                </p>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className={styles.codeSampleBlock}>
+          <div className={styles.secondaryHeading}>Code Sample</div>
+          <p>Ever wondered how to write a function that prints Hello World?</p>
+          <button onClick={() => setIsModalOpen(true)}>Show Me</button>
+          <CodeSampleModal
+            isOpen={isModalOpen}
+            closeModal={() => setIsModalOpen(false)}
+          />
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=learn&&utm_campaign=core-web-vitals"
+          target="_blank"
+        >
+         
+          <span className={styles.logo}>
+            <img src="/vercel.svg" />
+          </span>
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  return {
+    props: {
+      countries: countries.map((country) => ({
+        name: country.name,
+        cca2: country.cca2,
+        population: country.population,
+      })),
+    },
+  };
+}
